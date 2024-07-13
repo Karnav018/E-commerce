b6ac3b45b84a3315e92a25c5384fd681c546438a
Karnav018 <gandivdharikarnav@gmail.com>
2024-07-13 15:54:04 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { ProductDetails } from "./pages/ProductDetails";
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return <div className="overflow-hidden">react App</div>;
 };
 
 export default App;
